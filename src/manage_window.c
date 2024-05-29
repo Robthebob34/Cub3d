ac8bb18a041d9049eec0c0ca4975ad30fc574dfd
Robin Heck <rheck@fu-r9-p5.s19.be>
2024-05-29 15:13:31 +0200

    rgb to int

--- src/manage_window.c
+++ src/manage_window.c
@@ -6,11 +6,22 @@
 /*   By: rheck <rheck@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 12:58:22 by rheck             #+#    #+#             */
-/*   Updated: 2024/05/28 12:59:19 by rheck            ###   ########.fr       */
+/*   Updated: 2024/05/29 15:13:13 by rheck            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/cub3d.h"
+
+int rgb_to_int(double r, double g, double b)
+{
+    int color;
+
+	color = 0;
+    color |= (int)(b * 255);
+    color |= (int)(g * 255) << 8;
+    color |= (int)(r * 255) << 16;
+    return (color);
+}
 
 int	destroy_window(t_db *data)
 {
